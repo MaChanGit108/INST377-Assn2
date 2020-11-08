0e067b3b3c937f6566dda5a0f96d029c51d69bfb
MaChanGit108 <50186087+MaChanGit108@users.noreply.github.com>
2020-11-07 20:29:34 -0500

    Working filter, changed alignments, colors, reorganized code

--- public/script.js
+++ public/script.js
@@ -1,40 +1,43 @@
-function findMatches(wordToMatch, restaurants){
-    console.log(restaurants)
-    return restaurants.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.category.match(regex) || place.name.match(regex)
-    });
+const endpoint =
+  "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+
+const restaurants = [];
+
+fetch(endpoint)
+  .then((blob) => blob.json())
+  .then((data) => restaurants.push(...data));
+
+function findMatches(wordToMatch, restaurants) {
+  return restaurants.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.category.match(regex) || place.name.match(regex);
+  });
 }
 
-function displayMatches(restaurants){
-    const matchArray = findMatches(this.value, restaurants);
-    const html = matchArray.map(place => {
-        return `
-          <li>
-            <span class="name">${place.name}</span><br />
-            <span class="category">${place.category}</span><br />
-            <address class="address">
+function displayMatches() {
+  const matchArray = findMatches(this.value, restaurants);
+  const html = matchArray.map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const restaurantName = place.name.replace(regex, `<span class="h1">${this.value}</span>`);
+      return `
+           <li>
+             <span class="name">${restaurantName}</span><br />
+             <span class="category">${place.category}</span><br />
+             <address class="address">
                 ${place.address_line_1}<br />
-                ${place.city}<br />
-                ${place.zip}
+                 ${place.city}<br />
+                 ${place.zip}
             </address>
-          </li>
-        `;
-    }).join('');
-    const suggestions = document.querySelector('.suggestions');
-    suggestions.innerHTML = html;
+           </li>
+         `;
+    })
+    .join("");
+//   const suggestions = document.querySelector(".suggestions");
+  suggestions.innerHTML = html;
 }
 
-const searchInput = document.querySelector('.userinput');
+const userinputs = document.querySelector(".userinput");
+const suggestions = document.querySelector(".suggestions");
 
-async function loaddata() {
-    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-    const restaurants = []
-    const data = await fetch(endpoint);
-    const resj = await data.json();
-    displayMatches(resj)
-    searchInput.addEventListener('keyup', displayMatches);
-}
-
-window.onload = loaddata;
-
+// userinputs.addEventListener("change", displayMatches);
+userinputs.addEventListener("keypress", displayMatches);
